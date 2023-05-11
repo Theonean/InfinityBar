4087a5a82d0471ba96b59d80e5e98fbeaaaa5a12
Theonean <robinwelmers@protonmail.com>
2023-05-11 09:47:35 +0200

    did stuff

---
+++ BarData.js
@@ -0,0 +1,109 @@
+/*
+DirectionGod int to direction table
+ * 0: top, 1: bottom, 2: left, 3: right
+
+*/
+
+
+// JSON data structure
+var bardata = [
+    {
+        "parent": "wrapper",
+        //"startDir": "left", starting direction is determined by usersetting Chaos/Satisfying
+        "prefferedStartDir": 0,
+        "endPos": [0, 0],
+        "id": "wall"
+        //"className": "my-class",
+    },
+    {
+        "parent": "wrapper",
+        "prefferedStartDir": 1,
+        "endPos": [0, 0],
+        "id": "floor"
+        //"className": "my-class",
+    },
+    {
+        "parent": "wrapper",
+        "prefferedStartDir": 2,
+        "endPos": [60, 200],
+        "id": "counter"
+        //"className": "my-class",
+    },
+    {
+        "parent": "wrapper",
+        "prefferedStartDir": 2,
+        "endPos": [100, 240],
+        "id": "barstool"
+        //"className": "my-class",
+    },
+    {
+        "parent": "wrapper",
+        "prefferedStartDir": 2,
+        "endPos": [70, 180],
+        "id": "bubbleshooter"
+        //"className": "my-class",
+    },
+    {
+        "parent": "wrapper",
+        "prefferedStartDir": 2,
+        "endPos": [150, 106],
+        "id": "bottle1",
+        "className": "bottle"
+    },
+    {
+        "parent": "wrapper",
+        "prefferedStartDir": 2,
+        "endPos": [182, 106],
+        "id": "bottle2",
+        "className": "bottle"
+    },
+    {
+        "parent": "wrapper",
+        "prefferedStartDir": 2,
+        "endPos": [210, 106],
+        "id": "bottle3",
+        "className": "bottle"
+    },
+    {
+        "parent": "wrapper",
+        "prefferedStartDir": 2,
+        "endPos": [150, 78],
+        "id": " can1",
+        "className": "can"
+    },
+    {
+        "parent": "wrapper",
+        "prefferedStartDir": 2,
+        "endPos": [180, 78],
+        "id": " can2",
+        "className": "can"
+    },
+    {
+        "parent": "wrapper",
+        "prefferedStartDir": 2,
+        "endPos": [210, 78],
+        "id": " can3",
+        "className": "can"
+    },
+    {
+        "parent": "wrapper",
+        "prefferedStartDir": 2,
+        "endPos": [240, 78],
+        "id": " can4",
+        "className": "can"
+    },
+    {
+        "parent": "wrapper",
+        "prefferedStartDir": 2,
+        "endPos": [270, 78],
+        "id": " can5",
+        "className": "can"
+    },
+    {
+        "parent": "wrapper",
+        "prefferedStartDir": 2,
+        "endPos": [120, 78],
+        "id": " can6",
+        "className": "can"
+    }
+];
